51c216c09f24ca40ee67fe0ce7b3f48d7fa70501
tranduythanh <coachtranduythanh@gmail.com>
2023-05-08 09:10:43 +0700

    n=3

--- btreen/btree_test.cpp
+++ btreen/btree_test.cpp
@@ -406,3 +406,18 @@ TEST(BTreeTest, Prob1c) {
     }
     tree->Draw();
 }
+
+TEST(BTreeTest, Prob2a) {
+    auto tree = make_unique<BTreeN<int>>(3);
+    // Insert items
+    const std::array<int, 40> items = {
+        40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 
+        30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 
+        20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 
+        10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+    };
+    for (const auto& item : items) {
+        tree->Insert(item);
+    }
+    tree->Draw();
+}
